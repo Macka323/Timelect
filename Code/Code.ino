f50b30cc2ac447c2af5f55d93ec7704d12045282
Macka323 <75259519+Macka323@users.noreply.github.com>
2021-05-13 01:51:29 +0200

    Update Code.ino

--- Code/Code.ino
+++ Code/Code.ino
@@ -4,6 +4,7 @@
 CRGB leds[NUM_LEDS];
 int time;
 int display[4];
+int r=0,g=50,b=0;
 
 void setup() {
   FastLED.addLeds<NEOPIXEL, DATA_PIN>(leds, NUM_LEDS);
@@ -17,10 +18,54 @@ void loop() {
 
     time = Serial.parseInt();
   }
+ 
 
-  int number, y;
+  display[0]= time /1000;
+  display[1]= time /100%10;
+  display[2]= time %100/10;
+  display[3]= time %10;
 
-  number = time;
+Serial.print("display 0 - ");
+Serial.println(display[0]);
+Serial.print("display 1 - ");
+Serial.println(display[1]);
+Serial.print("display 2 - ");
+Serial.println(display[2]);
+Serial.print("display 3 - ");
+Serial.println(display[3]);
+
+if(display[0]=0){
+  leds[numb(1)].setRGB( r, g, b);
+  leds[numb(2)].setRGB( r, g, b);
+  leds[numb(21)].setRGB( r, g, b);
+  leds[numb(42)].setRGB( r, g, b);
+  leds[numb(84)].setRGB( r, g, b);
+  leds[numb(105)].setRGB( r, g, b);
+  leds[numb(127)].setRGB( r, g, b);
+  leds[numb(128)].setRGB( r, g, b);
+  leds[numb(108)].setRGB( r, g, b);
+  leds[numb(87)].setRGB( r, g, b);
+  leds[numb(24)].setRGB( r, g, b);
+  leds[numb(45)].setRGB( r, g, b);
+}
+if(display[0]=1){
+  leds[numb(24)].setRGB( r, g, b);
+  leds[numb(45)].setRGB( r, g, b);
+  leds[numb(87)].setRGB( r, g, b);
+  leds[numb(108)].setRGB( r, g, b);
+}else{
+  for(int i=0;i<NUM_LEDS;i++){
+    leds[i].setRGB( 0, 0, 0);
+  }
+}
+
+
+FastLED.show();
+}
+
+int numb(int number){
+  int y;
+
   if (number >= 0 && number <= 20) {
     y = map(number, 20, 0, 0, 20);
   }
@@ -36,27 +81,5 @@ void loop() {
     y=number;
   }
 
-  Serial.println(time);
-  Serial.println(y);
-
-  delay(700);
-
-  
-
-  display[0]= time /1000;
-  display[1]= time /100%10;
-  display[2]= time %100/10;
-  display[3]= time %10;
-
-/*Serial.print("display 0 - ");
-Serial.println(display[0]);
-Serial.print("display 1 - ");
-Serial.println(display[1]);
-Serial.print("display 2 - ");
-Serial.println(display[2]);
-Serial.print("display 3 - ");
-Serial.println(display[3]);*/
-
-
-
+  return y;
 }
