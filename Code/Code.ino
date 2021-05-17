abebeace994897679f352664f1122115e479a908
Macka323 <75259519+Macka323@users.noreply.github.com>
2021-05-17 09:16:42 +0200

    Update Code.ino

--- Code/Code.ino
+++ Code/Code.ino
@@ -4,6 +4,7 @@
 CRGB leds[NUM_LEDS];
 int time;
 int display[4];
+int display_last[4];
 
   int r = 0, g = 50, b = 0;
 
@@ -19,7 +20,8 @@ void loop() {
       time = Serial.parseInt();
       for(int i=0;i<NUM_LEDS;i++){
   leds[i].setRGB(0, 0, 0);
-}
+  
+}FastLED.show();
   }
 
 
@@ -37,12 +39,48 @@ void loop() {
   Serial.print("display 3 - ");
   Serial.println(display[3]);
 
-
+if(display_last[0]==display[0]){
+  r=0;
+  b=0;
+  g=0;
+}
+else{
+  g=50;
+}
+if(display_last[1]==display[1]){
+  r=0;
+  b=0;
+  g=0;
+}
+else{
+  g=50;
+}
+if(display_last[2]==display[2]){
+  r=0;
+  b=0;
+  g=0;
+}
+else{
+  g=50;
+}
+if(display_last[3]==display[3]){
+  r=0;
+  b=0;
+  g=0;
+}
+else{
+  g=50;
+}
 
   numb(0, display[0]);
   numb(1, display[1]);
   numb(2, display[2]);
   numb(3, display[3]);
+
+  display_last[0]=display[0];
+  display_last[1]=display[1];
+  display_last[2]=display[2];
+  display_last[3]=display[3];
 
 
 
