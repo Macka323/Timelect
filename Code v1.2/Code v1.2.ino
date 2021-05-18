6b4889931fc8596314f33f9a8ff22b043ff9b9d8
Macka323 <75259519+Macka323@users.noreply.github.com>
2021-05-18 20:48:57 +0200

    *

--- Code v1.2/Code v1.2.ino
+++ Code v1.2/Code v1.2.ino
@@ -1,16 +1,23 @@
 #include <FastLED.h>
+#define NUM_LEDS 147
+#define DATA_PIN 3
+CRGB leds[NUM_LEDS];
 
-int x[5];
+
+int x[5],xl[5];
 char y;
+int r=0,g=0,b=0;
+
 void setup() {
   // put your setup code here, to run once:
   Serial.begin(9600);
-  pinMode(13,OUTPUT);
+  pinMode(13, OUTPUT);
+  FastLED.addLeds<NEOPIXEL, DATA_PIN>(leds, NUM_LEDS);
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
- for(;;){
+  
     y = Serial.read();
     if (y == 'a') {
       x[0] = Serial.read() - 48;
@@ -25,7 +32,44 @@ void loop() {
       Serial.println(x[2]);
       Serial.print("x4-");
       Serial.println(x[3]);
+
+      if (xl[0] != x[0]) {
+        r = 0;
+        b = 0;
+        g = 0;
+      } else {
+        g = 50;
+      }
+      if (xl[1] != x[1]) {
+        r = 0;
+        b = 0;
+        g = 0;
+      } else {
+        g = 50;
+      }
+      if (xl[2] != x[2]) {
+        r = 0;
+        b = 0;
+        g = 0;
+      } else {
+        g = 50;
+      }
+      if (xl[3] != x[3]) {
+        r = 0;
+        b = 0;
+        g = 0;
+      } else {
+        g = 50;
+      }
     }
-    break;
- }
+    
+  numb(0, x[0]);
+  numb(1, x[1]);
+  numb(2, x[2]);
+  numb(3, x[3]);
+  xl[0] = x[0];
+  xl[1] = x[1];
+  xl[2] = x[2];
+  xl[3] = x[3];
+
 }
